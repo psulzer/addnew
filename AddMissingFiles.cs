52bb094fffd1bcb981d6f2fa1bce3c600719b7a1
hps <git@farout>
2016-07-01 12:52:38 +0200

    First (initial) commit

---
+++ AddMissingFiles.cs
@@ -0,0 +1,241 @@
+﻿// This file is part of addnew, Copyright © 2015 Peter Sulzer
+// for license information see end of this file
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+
+namespace addnew {
+
+class AddMissingFiles {
+    commandLineResult cmdrslt;
+    int filesCopied;
+    int dirsProcessed;
+    int dirsCreated;
+    public static string[] ERRMSG = {
+        // WARNING: Whenever you append another error message you must also append
+        // an error type to the enum ERR below (it would be better to use a struct
+        // with int and string, but then we must implement System.Collection.Ienumerable)
+        // so that we can initialise it with = { new {0,"no error}, ... }
+         "no error"         // 0
+        ,"file not found"   // 1
+        ,"no permission"    // 2
+        ,"invalid pathname" // 3
+        ,"problem with pathname (e. g. does not exist)" // 5
+        ,"this is a directory in source, but a file in destination" // 6
+        ,"creating directory in destination failed" // 7
+        ,"unexpected error destination directory does not exist" // 8
+        ,"unexpected error at FileInfo()" // 9
+        ,"unexpected error at File.Copy()" // 10
+        ,"unexpected error at Directory.GetFileSystemEntries()" // 11
+    };
+    public enum ERR {
+         NO_ERROR
+        ,SOURCE_DIR_NOT_FOUND
+        ,NO_PERMISSION
+        ,INVALID_PATHNAME
+        ,PROBLEM_WITH_PATH
+        ,DIR_IN_SOURCE_FILE_IN_DEST
+        ,CREATE_DEST_DIR_FAILED
+        ,UNEXPECTED_DEST_DIR_NOT_FOUND
+        ,UNEXPECTED_ERR_FILE_INFO
+        ,UNEXPECTED_ERR_FILE_COPY
+        ,UNEXPECTED_ERR_GETFILESYSTEMENTRIES
+    };
+
+    public AddMissingFiles(commandLineResult crslt) {
+        cmdrslt=crslt;
+        filesCopied=0;
+        dirsProcessed=0;
+        dirsCreated=0;
+    }
+
+    public int FilesCopied { get { return filesCopied; } }
+    public int DirsProcessed { get { return dirsProcessed; } }
+    public int DirsCreated { get { return dirsCreated; } }
+
+    public int addFiles(string[] de) { // recursive function!
+        // Console.WriteLine("Now the files will be added, once the program is finished\n");
+        int rslt=0;
+        string currd=cmdrslt.currd; // we need a local copy because of this function is recursive!
+        foreach (string path in de) {
+            cmdrslt.curr=path;
+            if (cmdrslt.debug >= 1)
+                Console.WriteLine("In addFiles(string[] de) in foreach-loop after cmdrslt.curr=path; path=\n{0}",
+                        cmdrslt.curr);
+            if (Directory.Exists(cmdrslt.curr)) {
+                if (!cmdrslt.recurse) // -recursesubdirs not given
+                    continue; // so ignore directory entries
+                //cmdrslt.curr=Path.GetDirectoryName(cmdrslt.curr)+@"\"+
+                //        Path.GetFileName(cmdrslt.curr);
+                if (cmdrslt.currd == "" || cmdrslt.currd == @"\" ||
+                        cmdrslt.currd.Substring(cmdrslt.currd.Length-1) == ":")
+                    cmdrslt.currd+=Path.GetFileName(cmdrslt.curr);
+                else {
+                    if (cmdrslt.debug >= 1)
+                        Console.WriteLine("In addFiles(string[] de) before cmdrrslt.currd+="+
+                            "@\"\\\"+..., cmdrslt.currd:\n{0}",cmdrslt.currd);
+                    cmdrslt.currd+=@"\"+Path.GetFileName(cmdrslt.curr); // H E R E  may be a problem
+                }
+                // Console.WriteLine("-----------{0}",Directory.GetCurrentDirectory());
+                if (!Directory.Exists(cmdrslt.currd)) {
+                    if (File.Exists(cmdrslt.currd))
+                        return -(int)ERR.DIR_IN_SOURCE_FILE_IN_DEST;
+                    if (cmdrslt.hot) {
+                        try {
+                            Directory.CreateDirectory(cmdrslt.currd);
+                        } catch {
+                            return -(int)ERR.CREATE_DEST_DIR_FAILED;
+                        }
+                    }
+                    dirsCreated++;
+                }
+                string[] de2;
+                try {
+                    de2=Directory.GetFileSystemEntries(cmdrslt.curr);
+                } catch {
+                    return (int)ERR.UNEXPECTED_ERR_GETFILESYSTEMENTRIES;
+                }
+                dirsProcessed++;
+                if (de2.Length > 0) {
+                    rslt=addFiles(de2);
+                    // cmdrslt.currd=currd; // restore old destination
+                    if (rslt < 0)
+                        return rslt;
+                }
+                cmdrslt.currd=currd; // restore old destination
+                continue;
+            }
+            if (!File.Exists(cmdrslt.curr)) { // if error
+                return -(int)ERR.PROBLEM_WITH_PATH; // error messages must be negative
+            }
+            // Now we have found a file. Next we must check if if
+            // the file exist in destination directory.
+            // If it exists and it is older print a message.
+            // If it doesn't exist, copy the file
+
+            string destpath="";
+            if (cmdrslt.currd == "" || cmdrslt.currd == @"\" ||
+                    (cmdrslt.currd.Substring(cmdrslt.currd.Length-1) == ":" && cmdrslt.currd.Length == 2))
+                destpath=cmdrslt.currd+Path.GetFileName(cmdrslt.curr);
+            else
+                destpath+=cmdrslt.currd+@"\"+Path.GetFileName(cmdrslt.curr);
+            if (cmdrslt.debug >= 1)
+                Console.WriteLine("In addFiles(string[] de) before GetFullPath(destpath), destpath:\n{0}",
+                        destpath);
+            destpath=Path.GetDirectoryName(Path.GetFullPath(destpath));
+            if (!Directory.Exists(destpath)) {
+                // A   T R Y   at Tue 08 Sep 2015 (seems to work - but just 1 test):
+                if (cmdrslt.hot) {
+                    Console.WriteLine("Error {0}: \"{1}\" {2}",(int)ERR.UNEXPECTED_DEST_DIR_NOT_FOUND,
+                        cmdrslt.currd,ERRMSG[(int)ERR.UNEXPECTED_DEST_DIR_NOT_FOUND]);
+                    return -(int)ERR.UNEXPECTED_DEST_DIR_NOT_FOUND; // WARNING: This return NOT TESTED (difficult)
+                }
+            }
+            else  {
+
+              // H I E R   W U R D E   E T W A S   N I C H T   D U R C H D A C H T
+              // Warum ??? Ist mir nicht mehr klar. Evtl. hinfällig dank Einführung der automatischen
+              // Variablen curr und currd in addFiles() (diese sind lokal zu jeder rekursiven Instanz
+              // von addFile(), sind nach der Rückkehr also wieder auf dem alten Wert und cmdrslt.currd
+              // wird unmittelbar nach dem rekursiven Aufruf von addFiles() wieder auf currd gesetzt.
+              // Diesen Kommentar löschen, wenn ausgetestet.
+
+                string f=destpath+"\\"+Path.GetFileName(cmdrslt.curr);
+                if (!File.Exists(f)) {
+                    // --------------
+                    // File does NOT exist in destination directory, so copy it:
+                    // --------------
+                    // Console.WriteLine("Processing {0}\n  Dest-Dir.: {1}",
+                    //        cmdrslt.curr,destpath);
+                    if (cmdrslt.hot) {
+                        try {
+                            // To test the "catch" below uncomment the following line:
+                            // throw new UnauthorizedAccessException();
+                            File.Copy(cmdrslt.curr,f,true); // Because of "true" this is not Multitasking safe!
+                        } catch (Exception e) {
+                            cmdrslt.except=e;
+                            return -(int)ERR.UNEXPECTED_ERR_FILE_COPY;
+                        }
+                    }
+                    else {
+                        if (!cmdrslt.dontprintcopied)
+                            Console.WriteLine("\"{0}\" will be copied to \"{1}\" with -HOT",
+                                cmdrslt.curr,destpath+@"\");
+                    }
+                    filesCopied++;
+                }
+                else {                      // file exists in destination directory:
+                    // ----------------
+                    //File exists in destination directory, so look what to do:
+                    // -----------------
+                    FileInfo fis,fid;
+                    try {
+                        // To test the "catch" below uncomment the following line:
+                        // throw new UnauthorizedAccessException();
+                        fid=new FileInfo(f);
+                        fis=new FileInfo(cmdrslt.curr);
+                    } catch (Exception e) {
+                        cmdrslt.except=e;
+                        return -(int)ERR.UNEXPECTED_ERR_FILE_INFO;
+                    }
+                    if (fid.LastWriteTime != fis.LastWriteTime || fid.Length != fis.Length) {
+                        Console.WriteLine("\"{0}\" not copied(!), but date or length differ",fis);
+                    }
+                }
+            }
+        } // foreach (string path in de) {
+        return 0;
+    }
+
+    int getDirEntries(string[] de) {
+        try {
+            // Obtain the file system entries in the directory path.
+            de=Directory.GetFileSystemEntries(cmdrslt.src+@"\"); 
+        }
+        catch (ArgumentNullException) {
+            // System.Console.WriteLine("Path is a null reference.");
+            return (int)ERR.SOURCE_DIR_NOT_FOUND;
+        }
+        catch (System.Security.SecurityException) {
+            // System.Console.WriteLine("The caller does not have the " +
+            //    "required permission.");
+            return (int)ERR.NO_PERMISSION;
+        }
+        catch (ArgumentException) {
+            // System.Console.WriteLine("Path is an empty string, " +
+            //    "contains only white spaces, " + 
+            //    "or contains invalid characters.");
+            return (int)ERR.INVALID_PATHNAME;
+        }
+        catch (System.IO.DirectoryNotFoundException) {
+            // System.Console.WriteLine("The path encapsulated in the " + 
+            //    "Directory object does not exist.");
+            return (int)ERR.PROBLEM_WITH_PATH;
+        }
+        return 0;
+}
+
+} // class AddMissingFiles
+
+} // namespace addnew
+
+//This file is part of addnew - add new files from one directory to another
+
+//Copyright © 2015 Peter Sulzer
+
+//"addnew" is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 1 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ Program.cs
@@ -0,0 +1,275 @@
+﻿// This file is part of addnew, Copyright © 2015 Peter Sulzer
+// for license information see end of this file
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace addnew {
+
+class commandLineResult {
+    public static string hotOpt="HOT"; // Sets hot true, upper case, so that we don't set it by accident
+    public static string dontprintcopiedOpt="dontprintcopied"; // Set dontprintcopied below to true
+    public static string recurseOpt="recursesubdirs"; // recurse into subdirectories
+    public static string copyrightOpt="copyright"; // show copyright/license information
+    public static string debugOpt="debug"; // print debugging informations
+    public int err; // returns an error number or 0 if no error
+    public int ndx; // returns current index of command line arguments
+    public int argc; // number of arguments (if errorr no. of argument where error occured)
+    public int optc; // number of options (if error no. of option where error occured)
+    public string src // source directory (may optionaly preceed filepattern below)
+        ,filepattern; // filename pattern, may contain wildcards (required, part after last '\' of full pathname)
+    // all other command arguments are optional
+    public string dst; // destination directory
+    public bool hot=false; // option hot - WARNING: When true (set) program is HOT and copies files
+    public bool dontprintcopied=false; // option -dontprintcopied, won't print files which would be copied
+    public bool recurse=false; // option -recursesubdirs - recursively act on all subdirectoriese
+    public bool copyright=false; // if true print copyright and license information for addnew
+    public int debug=0; // if debug > 0 print debugging informations
+    public string curr; // current pathname (source)
+    public string currd; // current pathname (destination)
+    public Exception except; // last exception (which led to error)
+    public int fileno,dirno,dircrea;
+
+    public commandLineResult() {
+        err=0; argc=0; optc=0; src=""; filepattern=""; dst="";
+        curr=""; currd=""; fileno=0; dirno=0; dircrea=0;
+    }
+};
+
+
+class Program {
+    const int
+         ERR_NOT_FOUND=6
+        ,ERR_SYNTAX=7
+        ,ERR_BAD_PARAMETER=12
+        ,ERR_NOT_IMPLEMENTED=17
+        ,ERR_DEST_IN_SOURCE=101
+        ,ERR_NO_SUCH_OPT=102
+        ;
+    const string VERSION="1.6";
+
+    static int Main(string[] args) {
+    // return 1: Called with bad parameter (arguments)
+    // return 2: Source or destination directory does not exist
+    Program an=new Program(); // an stands for addnew (name of the program)
+
+    commandLineResult cmdrslt=an.getCommandLineArguments(args);
+    if (cmdrslt.copyright)
+        an.printCopy();
+    switch (cmdrslt.err) {
+    case 0: // no error
+        break;
+    case ERR_NO_SUCH_OPT:
+        Console.WriteLine("Command line argument[{0}] \"{1}: No such option",cmdrslt.ndx+1,args[cmdrslt.ndx]);
+        return 1;
+    default:
+        Console.WriteLine("error when processing commandline, error number={0}\n",cmdrslt.err);
+        return 1;
+    }
+    if (cmdrslt.filepattern == "" || cmdrslt.filepattern == null || cmdrslt.filepattern == string.Empty) {
+        Console.WriteLine("addnew - copyright © 2015 Peter Sulzer\n");
+        Console.WriteLine("addnew V{0}\nUsage: addnew [-Option ...] filename [destination_directory]",VERSION);
+        Console.WriteLine(" Options:");
+        Console.WriteLine("  -HOT         create/copy directories/files, without this option\n"+
+                          "               program just prints what it would be doing with -HOT");
+        Console.WriteLine("  -r[ecursesubdirs] recurse into subdirectories");
+        Console.WriteLine("  -d[ontprintcopied] don't print out the files which\n"+
+                          "               would be copied (without -HOT)");
+        Console.WriteLine("  -c[opyright] show copyright and license information for this program");
+        Console.WriteLine("  -de[bug]     print debugging information (for development)");
+        return 1;
+    }
+    cmdrslt.src=cmdrslt.src.Replace('/','\\'); // addnew supports also '/' as directory separator
+    if (cmdrslt.dst.Length > 0)
+        cmdrslt.dst=cmdrslt.dst.Replace('/','\\'); // addnew supports also '/' as directory separator
+    // Comment following statement after testing:
+    // Console.WriteLine("err={0}\nargc={1}\noptc={2}\nsrc=\"{3}\"\nfilepattern=\"{4}\"\ndst=\"{5}\"\n"+
+    //    "opt=\"n{0}\"\ncurr=\"{6}\"\ncurrd=\"{7}\"",
+    //    cmdrslt.err,cmdrslt.argc,cmdrslt.optc,cmdrslt.src,cmdrslt.filepattern,cmdrslt.dst,
+    //    cmdrslt.hot,cmdrslt.curr,cmdrslt.currd);
+
+    if (!Directory.Exists(cmdrslt.src)) {
+        Console.WriteLine("Source directory \"{0}\" does not exist",cmdrslt.src);
+        return 2;
+    }
+    if (cmdrslt.dst != "" && !Directory.Exists(cmdrslt.dst)) {
+        Console.WriteLine("Destination Directory \"n{0}\" does not exist",cmdrslt.dst);
+        return 2;
+    }
+    string fpd=".";
+    if (cmdrslt.dst != "")
+        fpd=Path.GetFullPath(cmdrslt.dst); //                                         |
+    if (Path.GetFullPath(fpd).StartsWith(Path.GetFullPath(cmdrslt.src))) { //<---+
+        Console.WriteLine("Error {0}: Destination directory is contained in source directory.\n"+
+                "Recursion is not allowed (results in endless loop)",ERR_DEST_IN_SOURCE);
+        return 2;
+    }
+
+    // cmdrslt.curr=cmdrslt.src;
+    if (cmdrslt.dst.Length > 1 && cmdrslt.dst.Substring(cmdrslt.dst.Length-1) == @"\")
+        cmdrslt.currd=cmdrslt.dst.Substring(0,cmdrslt.dst.Length-1);
+    else
+        cmdrslt.currd=cmdrslt.dst;
+    AddMissingFiles amf=new AddMissingFiles(cmdrslt);
+    string[] de=Directory.GetFileSystemEntries(cmdrslt.src,cmdrslt.filepattern);
+    if (de.Length > 0) {
+
+        // Now copy (add) new files (create new directories) recursively:
+        int rslt=amf.addFiles(de);
+
+        if (rslt < 0) {
+            switch (rslt) {
+            case (-(int)AddMissingFiles.ERR.UNEXPECTED_ERR_FILE_INFO):
+                Console.WriteLine("Error {0}: \"{1}\" at file \"{2}\"\nException: {3}",
+                    AddMissingFiles.ERR.UNEXPECTED_ERR_FILE_INFO,cmdrslt.curr,cmdrslt.except);
+                break;
+            case (-(int)AddMissingFiles.ERR.UNEXPECTED_ERR_FILE_COPY):
+                Console.WriteLine("Error {0}: \"{1}\" at \"{2}\"\nException: {3}",
+                    -rslt,AddMissingFiles.ERRMSG[-rslt],cmdrslt.curr,cmdrslt.except);
+                break;
+            default:
+                Console.WriteLine("Error {0}: \"{1}\" {2}",
+                        -rslt,cmdrslt.curr,AddMissingFiles.ERRMSG[-rslt]);
+                break;
+            }
+        }
+
+        if (!cmdrslt.hot)
+            Console.WriteLine(
+                "\nWith option \"-HOT\" following no. of directories/files will be created/copied:");
+        else
+            Console.WriteLine();
+        Console.WriteLine("{0} directories processed (searched) - {1} directories created",
+                amf.DirsProcessed,amf.DirsCreated);
+        Console.WriteLine("{0} files copied",amf.FilesCopied);
+        if (rslt < 0)
+            return 3;
+        if (de.Length>0) return 0; // The if (...) just added to supress compiler warning
+    }
+    else {
+        Console.WriteLine("No files/directories found in \"{0}\"",cmdrslt.src);
+        return 0; // This is no error, all is correct, but there are no files to copy
+    }
+    return 0;
+} // Main()
+
+
+public commandLineResult getCommandLineArguments(string[] args) {
+    commandLineResult cmdrslt=new commandLineResult();
+    cmdrslt.argc=0; // argument counter (i. e. all arguments without leading "-", which are options)
+    cmdrslt.optc=0; // option counter, counts number of options
+    bool isopt=false; // true if current argument is an optioon
+    int argsSize=args.Length;
+    for (cmdrslt.ndx=0; cmdrslt.ndx < argsSize; cmdrslt.ndx++) {
+        if (args[cmdrslt.ndx].StartsWith("-")) {
+            isopt=true;
+            ++cmdrslt.optc;
+            cmdrslt.ndx=clrHandleOption(cmdrslt,args,cmdrslt.ndx);
+            if (cmdrslt.err != 0)
+                return cmdrslt;
+        }
+        else {
+            isopt=false;
+            ++cmdrslt.argc;
+            cmdrslt.ndx=clrHandleArgument(cmdrslt,args,cmdrslt.ndx);
+            if (cmdrslt.err != 0)
+                return cmdrslt;
+        }
+    }
+    return cmdrslt;
+}
+
+
+int clrHandleOption(commandLineResult cmdrslt,string[] args,int i) {
+    // cmdrslt.err=ERR_NOT_IMPLEMENTED;
+    string opt=args[i].Substring(1);
+    // switch (opt) { // possible in C#...
+    // case "HOT":   // ...but not in C/C++ so for portability used if()...else if() ...else()
+    if (opt == commandLineResult.hotOpt) {
+            cmdrslt.hot=true;
+    }
+    // Handle options starting with -d
+        // options may be abreviated e. g. -d instead of -dont... (don't forget to adapt the usage message!)
+        // currently there is only one option starting with 'd# so we must just test if
+        // the option starting with d is contained in dontprintcopiedOpt:
+    else if (opt.StartsWith("d")) {
+        if (commandLineResult.dontprintcopiedOpt.StartsWith(opt))
+            cmdrslt.dontprintcopied=true;
+        else if (commandLineResult.debugOpt.StartsWith(opt))
+            cmdrslt.debug=1;
+    }
+    else if (commandLineResult.recurseOpt.StartsWith(opt)) {
+        cmdrslt.recurse=true;
+    }
+    else if (commandLineResult.copyrightOpt.StartsWith(opt)) {
+        cmdrslt.copyright=true;
+    }
+    else {
+        cmdrslt.err=ERR_NO_SUCH_OPT;
+    }
+    return i;
+}
+
+
+int clrHandleArgument(commandLineResult cmdrslt,string[] args,int i) {
+    string s="";
+    switch (cmdrslt.argc) {
+    case 1:
+        s=Path.GetDirectoryName(args[i]);
+        if (s != null && s != string.Empty)
+            cmdrslt.src=s;
+        s=Path.GetFileName(args[i]);
+        if (s != null)
+            cmdrslt.filepattern=s;
+        else {
+            cmdrslt.err=ERR_BAD_PARAMETER;
+            return i;
+        }
+        return i;
+    case 2:
+        cmdrslt.dst=args[i]; // for destination only a directory is allowed
+        return i;
+    default:
+        cmdrslt.err=ERR_BAD_PARAMETER;
+        break;
+    }
+    return i;
+}
+
+void printCopy() {
+    Console.WriteLine("addnew - add new files from one directory to another\n\n"+
+                      "Copyright © 2015 Peter Sulzer\n");
+    Console.WriteLine("This program is free software: you can redistribute it and/or modify\n"+
+                      "it under the terms of the GNU General Public License as published by\n"+
+                      "the Free Software Foundation, either version 1 of the License, or\n"+
+                      "(at your option) any later version.\n");
+    Console.WriteLine("This program is distributed in the hope that it will be useful,\n"+
+                      "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"+
+                      "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"+
+                      "GNU General Public License for more details.\n");
+    Console.WriteLine("You should have received a copy of the GNU General Public License\n"+
+                      "along with this program.  If not, see <http://www.gnu.org/licenses/>.\n");
+}
+
+} // class Program
+} // namespace addnew
+
+//This file is part of addnew - add new files from one directory to another
+
+//Copyright © 2015 Peter Sulzer
+
+//"addnew" is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 1 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
